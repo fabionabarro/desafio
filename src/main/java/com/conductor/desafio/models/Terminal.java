467e399a1f91e9fe65ce1c5551fa7e5383692369
fabionabarro <fabionabarro@gmail.com>
2021-05-17 16:04:43 -0300

    Criando conexao banco postgres

---
+++ src/main/java/com/conductor/desafio/models/Terminal.java
@@ -0,0 +1,102 @@
+package com.conductor.desafio.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name="TB_TERMINAL")
+public class Terminal implements Serializable {
+    @Id
+    private Integer logic;
+    private String serial;
+    private String model;
+    private Integer sam;
+    private String ptid;
+    private Integer plat;
+    private String version;
+    private Integer mxr;
+    private Integer mxf;
+    private String  VERFM;
+
+    public Integer getLogic() {
+        return logic;
+    }
+
+    public void setLogic(Integer logic) {
+        this.logic = logic;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getSam() {
+        return sam;
+    }
+
+    public void setSam(Integer sam) {
+        this.sam = sam;
+    }
+
+    public String getPtid() {
+        return ptid;
+    }
+
+    public void setPtid(String ptid) {
+        this.ptid = ptid;
+    }
+
+    public Integer getPlat() {
+        return plat;
+    }
+
+    public void setPlat(Integer plat) {
+        this.plat = plat;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Integer getMxr() {
+        return mxr;
+    }
+
+    public void setMxr(Integer mxr) {
+        this.mxr = mxr;
+    }
+
+    public Integer getMxf() {
+        return mxf;
+    }
+
+    public void setMxf(Integer mxf) {
+        this.mxf = mxf;
+    }
+
+    public String getVERFM() {
+        return VERFM;
+    }
+
+    public void setVERFM(String VERFM) {
+        this.VERFM = VERFM;
+    }
+}
