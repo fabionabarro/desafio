97b1579b793e2895b1b18e8389898060d82c0fb2
fabionabarro <fabionabarro@gmail.com>
2021-05-19 15:28:39 -0300

    Metodos POST recebe apenas text/html + log

--- src/main/java/com/conductor/desafio/models/Terminal.java
+++ src/main/java/com/conductor/desafio/models/Terminal.java
@@ -1,11 +1,13 @@
 package com.conductor.desafio.models;
 
-import com.sun.istack.NotNull;
+import com.sun.istack.internal.NotNull;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
+
+
 
 @Entity
 @Table(name="TB_TERMINAL")
@@ -30,79 +32,95 @@ public class Terminal implements Serializable {
         return logic;
     }
 
-    public void setLogic(Integer logic) {
-        this.logic = logic;
-    }
-
     public String getSerial() {
         return serial;
-    }
-
-    public void setSerial(String serial) {
-        this.serial = serial;
     }
 
     public String getModel() {
         return model;
     }
 
-    public void setModel(String model) {
-        this.model = model;
-    }
-
     public Integer getSam() {
         return sam;
-    }
-
-    public void setSam(Integer sam) {
-        this.sam = sam;
     }
 
     public String getPtid() {
         return ptid;
     }
 
-    public void setPtid(String ptid) {
-        this.ptid = ptid;
-    }
-
     public Integer getPlat() {
         return plat;
-    }
-
-    public void setPlat(Integer plat) {
-        this.plat = plat;
     }
 
     public String getVersion() {
         return version;
     }
 
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
     public Integer getMxr() {
         return mxr;
-    }
-
-    public void setMxr(Integer mxr) {
-        this.mxr = mxr;
     }
 
     public Integer getMxf() {
         return mxf;
     }
 
-    public void setMxf(Integer mxf) {
-        this.mxf = mxf;
-    }
-
     public String getVERFM() {
         return VERFM;
     }
 
+    public void setLogic(Integer logic) {
+        this.logic = logic;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSam(Integer sam) {
+        this.sam = sam;
+    }
+
+    public void setPtid(String ptid) {
+        this.ptid = ptid;
+    }
+
+    public void setPlat(Integer plat) {
+        this.plat = plat;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public void setMxr(Integer mxr) {
+        this.mxr = mxr;
+    }
+
+    public void setMxf(Integer mxf) {
+        this.mxf = mxf;
+    }
+
     public void setVERFM(String VERFM) {
         this.VERFM = VERFM;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "logic:" + logic + "" +
+                ", serial:" + '\"' +  serial + '\"'  +
+                ", model:" + '\"' + model + '\"' +
+                ", sam:" + sam  +
+                ", ptid:" + '\"' + ptid + '\"' +
+                ", plat:" + plat +
+                ", version:" + '\"' + version + '\"' +
+                ", mxr:" + mxr +
+                ", mxf:" + mxf +
+                ", VERFM:"+ '\"'  + VERFM + '\"' +
+                '}';
     }
 }
--- src/main/java/com/conductor/desafio/repository/DesafioRepository.java
+++ src/main/java/com/conductor/desafio/repository/DesafioRepository.java
@@ -3,10 +3,7 @@ package com.conductor.desafio.repository;
 import com.conductor.desafio.models.Terminal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface DesafioRepository extends JpaRepository<Terminal, Integer>{
 
-    Optional<Terminal> findById(Integer logic);
-
+    Terminal findByLogic (Integer logic);
 }
