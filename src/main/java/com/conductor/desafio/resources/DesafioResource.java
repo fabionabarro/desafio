88ad2434eb1960438a1b4a40931cf86d851cecb8
fabionabarro <fabionabarro@gmail.com>
2021-05-19 15:28:39 -0300

    Metodos POST recebe apenas text/html + log

--- src/main/java/com/conductor/desafio/resources/DesafioResource.java
+++ src/main/java/com/conductor/desafio/resources/DesafioResource.java
@@ -2,14 +2,14 @@ package com.conductor.desafio.resources;
 
 import com.conductor.desafio.models.Terminal;
 import com.conductor.desafio.repository.DesafioRepository;
+import com.conductor.desafio.services.ConverterToJson;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
-
 
 @RestController
 @RequestMapping(value = "/v1")
@@ -19,24 +19,30 @@ public class DesafioResource {
     @Autowired
     DesafioRepository desafioRepository;
 
-    @GetMapping("/terminal")
-    @ApiOperation(value="Retorna lista de terminais")
-    public List<Terminal> listaTerminais(){
-        return desafioRepository.findAll() ;
-    }
+    final static Logger LOGGER = LoggerFactory.getLogger(DesafioResource.class);
+
     @GetMapping("/terminal/{logic}")
     @ApiOperation(value="Retorna um terminal unico")
-    public Optional<Terminal> Terminal(@PathVariable(value = "logic")Integer logic){
-        return desafioRepository.findById(logic) ;
+    public Terminal Terminal(@PathVariable(value = "logic")Integer logic){
+        return desafioRepository.findByLogic(logic);
     }
-    @PostMapping("/terminal")
-    @ApiOperation(value="Salva um terminal unico")
-    public Terminal salvaTerminal(@RequestBody Terminal terminal){
-        return desafioRepository.save(terminal);
-    }
+
     @PutMapping("/terminal")
     @ApiOperation(value="Atualiza um terminal unico passando parametro 'logic' ")
     public Terminal atualizaTerminal(@RequestBody Terminal terminal){
+        String json = terminal.toString();
+        LOGGER.info("Terminal object: {}", json);
         return desafioRepository.save(terminal);
     }
+
+    @PostMapping(value = "/terminal",consumes = MediaType.TEXT_HTML_VALUE)
+    @ApiOperation(value="Salva um terminal unico")
+    public String salvaTerminal(@RequestBody String terminalpost ) {
+        Terminal terminal = ConverterToJson.convertStringTerminal(terminalpost);
+        String json = terminal.toString();
+        LOGGER.info("Terminal object: {}", json);
+        desafioRepository.save(terminal);
+        return (json);
+    }
 }
+
